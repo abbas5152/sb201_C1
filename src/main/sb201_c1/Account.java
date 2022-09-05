d52c7a74d081f9a0f99c934cce1a2cfac4cb8fdf
Abbas Ghaniwala <abbasghaniwala92@gmail.com>
2022-09-05 12:11:43 +0530

    Create Account.java

---
+++ src/main/sb201_c1/Account.java
@@ -0,0 +1,55 @@
+package sb201_c1;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+
+	@Id
+	private int id;
+	private String email;
+	private String address;
+	private int balance;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(double d) {
+		this.balance = (int) d;
+	}
+	public Account(int id, String email, String address, int balance) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.address = address;
+		this.balance = balance;
+	}
+	public Account() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", email=" + email + ", address=" + address + ", balance=" + balance + "]";
+	}
+	
+	
+}
